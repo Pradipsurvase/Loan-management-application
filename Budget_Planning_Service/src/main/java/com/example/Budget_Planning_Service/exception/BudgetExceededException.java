7ba0176b84d7d380e140338c3025bb55042c4ce9
Madhurakhande <khandemadhura2003@gmail.com>
2026-03-15 14:47:09 +0530

    git action changes

--- Budget_Planning_Service/src/main/java/com/example/Budget_Planning_Service/exception/BudgetExceededException.java
+++
@@ -1,5 +0,0 @@
-package com.example.Budget_Planning_Service.exception;
-
-public class BudgetExceededException extends RuntimeException {
-    public BudgetExceededException(String message) { super(message); }
-}
