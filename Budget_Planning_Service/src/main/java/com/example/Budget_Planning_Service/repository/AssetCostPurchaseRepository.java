4bc8ebd9c48445ccaad6774d68ee6d520b965ef4
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-15 15:39:59 +0530

    github actions

--- Budget_Planning_Service/src/main/java/com/example/Budget_Planning_Service/repository/AssetCostPurchaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Budget_Planning_Service.repository;
-
-import com.example.Budget_Planning_Service.model.entity.AssetCostPurchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssetCostPurchaseRepository extends JpaRepository<AssetCostPurchase, Long> {
-}
